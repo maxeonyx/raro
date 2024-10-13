86608226254c8a6cb969ee30207e876d2eb11b7a
Max Clarke <maxeonyx@gmail.com>
2024-10-13 18:38:30 +1300

    WIP: parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,6 @@
+use std::borrow::Borrow;
+
+use aho_corasick::AhoCorasick;
 /// # Parser
 ///
 /// The idea of this language is to have "chaining" syntax like the following
@@ -44,51 +47,169 @@
 /// ```
 /// a scale(2) repeat{ count: 3 }
 /// ```
-
-use chumsky::{extra::Full, prelude::*};
+use chumsky::{extra::Full, prelude::*, primitive};
 
 #[derive(Debug, Clone)]
-struct BlockInner {
-    statements: Vec<Expr>,
-    expr: Option<Box<Expr>>,
+enum BracketedNode<'inp> {
+    Str(&'inp str),
+    Round(Vec<BracketedNode<'inp>>),
+    Square(Vec<BracketedNode<'inp>>),
+    Curly(Vec<BracketedNode<'inp>>),
+    Angle(Vec<BracketedNode<'inp>>),
+    InvalidRound(&'inp str),
+    InvalidSquare(&'inp str),
+    InvalidCurly(&'inp str),
+    InvalidAngle(&'inp str),
 }
 
+#[derive(Debug, Clone)]
+struct BlockInner<'inp> {
+    statements: Vec<Expr<'inp>>,
+    expr: Option<Box<Expr<'inp>>>,
+}
+
+#[derive(Debug, Clone)]
+enum Token {
+    PLUS,
+}
 
 #[derive(Debug, Clone)]
 enum Literal {
     Num(f64),
-    Str(String),
+    Str(&'static str),
     Bool(bool),
 }
 
 #[derive(Debug, Clone)]
-enum SimpleExpr {
-    Literal(Literal),
-    Var(String),
-    Paren(Expr),
+enum InfixOp {
+    Add, // +
+    Sub, // -
+    Mul, // *
+    Div, // /
+    Exp, // **
 }
 
 #[derive(Debug, Clone)]
-enum Expr {
-    InfixOp(Box<SimpleExpr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    Div(Box<Expr>, Box<Expr>),
+enum Expr<'inp> {
+    InfixOp(InfixOp, Box<Expr<'inp>>),
     Literal(Literal),
-    Var(String),
-    Assign(Box<Expr>, String),
-    Block(BlockInner),
+    Paren(Box<Expr<'inp>>),
+    Var(&'inp str),
+    Assign(Box<Location<'inp>>, Box<Expr<'inp>>),
+    Block(BlockInner<'inp>),
 }
 
+#[derive(Debug, Clone)]
+enum Location<'inp> {
+    Var(&'inp str),
+}
+
+#[derive(Debug, Clone)]
 struct ParserState {}
 
+#[derive(Debug, Clone)]
 struct ParserContext {}
 
-fn parser<'inp>(input: &'inp str) -> impl Parser<'inp, &'inp str, BlockInner, Full<Rich<'inp, char>, ParserState, ParserContext>> {
+const VAR: f64 = 1.156;
 
-    just("a").to(BlockInner {
-        statements: vec![],
-        expr: None,
-    })
+/// # Apply string processing rules.
+/// This takes the raw source code of the string and turns it into the string
+/// value in the program.
+///
+/// ## Rules
+/// - Replace escapes (\t etc.)
+/// - If the first char is a newline, strip it.
+/// - Replace leading whitespace up to the indent of the first standalone line.
+/// -
+///
+///
+// fn process_str<'inp>(s: &'inp str) -> String {
+//     let a = AhoCorasick::builder()
+//         .ascii_case_insensitive(false)
+//         .build(["\\t", "\\n", "\\r", "\\\"", "\\\\"])
+//         .unwrap();
+//     a.replace_all(s, &["\t", "\n", "\r", "\"", "\\"])
+// }
 
+fn parser<'inp>(
+) -> impl Parser<'inp, &'inp str, Expr<'inp>, Full<Rich<'inp, char>, ParserState, ParserContext>> + Clone
+{
+    let p_number = text::digits(10)
+        .to_slice()
+        .map(|s: &str| Expr::Literal(Literal::Num(s.parse().unwrap())));
+
+    let p_bool = choice((
+        just("true").map(|_| Expr::Literal(Literal::Bool(true))),
+        just("false").map(|_| Expr::Literal(Literal::Bool(false))),
+    ));
+
+    let mut p_expr = Recursive::declare();
+    let mut p_chain = Recursive::declare();
+    let mut p_simpleexpr = Recursive::declare();
+    let mut p_infix = Recursive::declare();
+
+    let parse_infix = |s: &'static str, op: InfixOp| {
+        just(s)
+            .ignore_then(p_simpleexpr)
+            .map(|(state, e)| Expr::InfixOp(op, Box::new(e)))
+    };
+
+    p_infix.define(choice((
+        parse_infix("+ ", InfixOp::Add),
+        parse_infix("- ", InfixOp::Add),
+        parse_infix("* ", InfixOp::Add),
+        parse_infix("/ ", InfixOp::Add),
+    )));
+
+    p_simpleexpr.define(just("(").ignore_then(p_expr).then_ignore(just(")")));
+
+    // TODO: Strings...
+    // escapes
+    // multiline & whitespace behaviour
+    // f-strings
+
+    p_chain.define(
+        p_expr
+            .repeated()
+            .separated_by(just(" "))
+            .collect::<Vec<_>>(),
+    );
+
+    p_chain
 }
+
+// fn parse_nonbracketed<'inp>() -> impl Parser<
+//     'inp,
+//     &'inp str,
+//     BracketedNode<'inp>,
+//     Full<Rich<'inp, char>, ParserState, ParserContext>,
+// > + Clone {
+//     primitive::none_of("()[]{}")
+//         .repeated()
+//         .to_slice()
+//         .map(BracketedNode::Str)
+// }
+
+// fn pb<'inp>() -> impl Parser<
+//     'inp,
+//     &'inp str,
+//     Vec<BracketedNode<'inp>>,
+//     Full<Rich<'inp, char>, ParserState, ParserContext>,
+// > {
+//     recursive(|pb_| {
+//         choice((
+//             parse_nonbracketed(),
+//             pb_.clone()
+//                 .delimited_by(just("{"), just("}"))
+//                 .map(BracketedNode::Curly),
+//             pb_.clone()
+//                 .delimited_by(just("["), just("]"))
+//                 .map(BracketedNode::Square),
+//             pb_.clone()
+//                 .delimited_by(just("("), just(")"))
+//                 .map(BracketedNode::Curly),
+//         ))
+//         .repeated()
+//         .collect()
+//     })
+// }
